02648383f6f05d13e057c42d59293a3f9678aa63
AsifKhan-27 <asifak001@gmail.com>
2022-08-23 21:59:20 +0600

    completed add-player.js

--- js/add-players.js
+++ js/add-players.js
@@ -19,16 +19,57 @@ function addPlayerToList(playerName){
 	playersList.appendChild(li);
 }
 
-
-
-document.getElementById('btn-p1').addEventListener('click',function(){
-	const playerNameElement=document.getElementById('name-p1');
+function addPlayer(buttonElement,playerNameElement){
 	const playerName=playerNameElement.innerText;
 
 	if (numberOfPlayers('player-list')<5){
+		buttonElement.disabled = true;
+		buttonElement.style.backgroundColor='grey';
 		addPlayerToList(playerName);
 	}
 	else{
 		alert("You have already selected 5 players");
 	}
+}
+
+document.getElementById('btn-p1').addEventListener('click',function(){
+	const buttonElement=document.getElementById('btn-p1');
+	const playerNameElement=document.getElementById('name-p1');
+	addPlayer(buttonElement,playerNameElement);
+	
+});
+
+document.getElementById('btn-p2').addEventListener('click',function(){
+	const buttonElement=document.getElementById('btn-p2');
+	const playerNameElement=document.getElementById('name-p2');
+	addPlayer(buttonElement,playerNameElement);
+	
+});
+
+document.getElementById('btn-p3').addEventListener('click',function(){
+	const buttonElement=document.getElementById('btn-p3');
+	const playerNameElement=document.getElementById('name-p3');
+	addPlayer(buttonElement,playerNameElement);
+	
+});
+
+document.getElementById('btn-p4').addEventListener('click',function(){
+	const buttonElement=document.getElementById('btn-p4');
+	const playerNameElement=document.getElementById('name-p4');
+	addPlayer(buttonElement,playerNameElement);
+	
+});
+
+document.getElementById('btn-p5').addEventListener('click',function(){
+	const buttonElement=document.getElementById('btn-p5');
+	const playerNameElement=document.getElementById('name-p5');
+	addPlayer(buttonElement,playerNameElement);
+	
+});
+
+document.getElementById('btn-p6').addEventListener('click',function(){
+	const buttonElement=document.getElementById('btn-p6');
+	const playerNameElement=document.getElementById('name-p6');
+	addPlayer(buttonElement,playerNameElement);
+	
 });
