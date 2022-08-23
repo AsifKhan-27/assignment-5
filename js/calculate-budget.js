3bc3d228e873b707356d5b30b6e9636524d89ba9
AsifKhan-27 <asifak001@gmail.com>
2022-08-23 22:33:51 +0600

    completed total calculation in calculate-budget.js

--- js/calculate-budget.js
+++ js/calculate-budget.js
@@ -1,10 +1,10 @@
 console.log('calculate-budget');
 
-function getPerPlayerBudget(){
-	const perPlayerInputElement=document.getElementById('per-player-budget');
-	const perPlayerInputString=perPlayerInputElement.value;
-	const perPlayerInput=parseInt(perPlayerInputString);
-	return perPlayerInput;
+function getBudget(elementId){
+	const inputElement=document.getElementById(elementId);
+	const inputString=inputElement.value;
+	const input=parseInt(inputString);
+	return input;
 }
 
 function calculatePlayerExpenses(perPlayerBudget){
@@ -13,14 +13,30 @@ function calculatePlayerExpenses(perPlayerBudget){
 	return playerExpenses;
 }
 
-function setPlayerExpenses(playerExpenses){
-	const playerExpensesElement=document.getElementById('player-expenses');
-	playerExpensesElement.innerText=playerExpenses;
+function setElementValue(elementId,value){
+	const element=document.getElementById(elementId);
+	element.innerText=value;
+}
+
+function calculateTotalBudget(players,manager,coach){
+	const total=players+manager+coach;
+	return total;
 }
 
 
 document.getElementById('calculate-btn').addEventListener('click',function(){
-	const perPlayerBudget=getPerPlayerBudget();
+	const perPlayerBudget=getBudget('per-player-budget');
 	const playerExpenses=calculatePlayerExpenses(perPlayerBudget);
-	setPlayerExpenses(playerExpenses);
+	setElementValue('player-expenses',playerExpenses);
+});
+
+document.getElementById('calculate-total-btn').addEventListener('click',function(){
+	const managerBudget=getBudget('manager-budget');
+	const coachBudget=getBudget('coach-budget');
+
+	const playerExpensesString=document.getElementById('player-expenses').innerText;
+	const playerExpenses=parseInt(playerExpensesString);
+
+	const totalBudget=calculateTotalBudget(playerExpenses,managerBudget,coachBudget);
+	setElementValue('total',totalBudget);
 });
